83aa9cced6417b062162f2a7c35101af7cf70ee0
ryandimba <ryan.dimba@student.moringaschool.com>
2022-08-29 09:12:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const stringArray = word.split('');
+  const reversedstringArray = stringArray.reverse();
+  const reversedWord = reversedstringArray.join('');
+
+  if (reversedWord === word) {
+    return true;
+
+}else{
+  return false;
+}
 }
 
 /* 
   Add your pseudocode here
+  create a new function that takes a string and returns  reversed string
+  write a function to reverse the string
+  write conditional statement
+  
 */
 
 /*
